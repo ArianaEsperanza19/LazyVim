f52d2bfb9f74b3f046b08e76541fbd94b705b038
ArianaEsperanza19 <ArianaEsperanza19>
2025-12-14 20:45:19 -0400

    Cambiar lualine, nuevos snippets de rust y nuevo tema cyberdream

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -5,56 +5,56 @@
 return {
   "nvim-lualine/lualine.nvim",
   dependencies = { "nvim-tree/nvim-web-devicons" },
-  opts = {
-    icons_enabled = true,
-    theme = "auto",
-    component_separators = { left = "", right = "" },
-    section_separators = { left = "", right = "" },
-    disabled_filetypes = {
-      statusline = {},
-      winbar = {},
-    },
-    ignore_focus = {},
-    always_divide_middle = true,
-    always_show_tabline = true,
-    globalstatus = false,
-    refresh = {
-      statusline = 100,
-      tabline = 100,
-      winbar = 100,
-    },
-    sections = {
-      lualine_a = { "mode" },
-      lualine_b = { "branch", "diff", "diagnostics" },
-      lualine_c = { "filename" },
-      lualine_x = {
-        "encoding",
-        "fileformat",
-        "filetype",
-        -- Agregamos la función personalizada aquí
-        function()
-          if vim.o.spell then
-            local spelllang = vim.api.nvim_buf_get_option(0, "spelllang")
-            return spelllang
-          else
-            return ""
-          end
-        end,
-      },
-      lualine_y = { "progress" },
-      lualine_z = { "location" },
-    },
-    inactive_sections = {
-      lualine_a = {},
-      lualine_b = {},
-      lualine_c = { "filename" },
-      lualine_x = { "location" },
-      lualine_y = {},
-      lualine_z = {},
-    },
-    tabline = {},
-    winbar = {},
-    inactive_winbar = {},
-    extensions = {},
-  },
+  -- opts = {
+  --   icons_enabled = true,
+  --   theme = "auto",
+  --   component_separators = { left = "", right = "" },
+  --   section_separators = { left = "", right = "" },
+  --   disabled_filetypes = {
+  --     statusline = {},
+  --     winbar = {},
+  --   },
+  --   ignore_focus = {},
+  --   always_divide_middle = true,
+  --   always_show_tabline = true,
+  --   globalstatus = false,
+  --   refresh = {
+  --     statusline = 100,
+  --     tabline = 100,
+  --     winbar = 100,
+  --   },
+  --   sections = {
+  --     lualine_a = { "mode" },
+  --     lualine_b = { "branch", "diff", "diagnostics" },
+  --     lualine_c = { "filename" },
+  --     lualine_x = {
+  --       "encoding",
+  --       "fileformat",
+  --       "filetype",
+  --       -- Agregamos la función personalizada aquí
+  --       function()
+  --         if vim.o.spell then
+  --           local spelllang = vim.api.nvim_buf_get_option(0, "spelllang")
+  --           return spelllang
+  --         else
+  --           return ""
+  --         end
+  --       end,
+  --     },
+  --     lualine_y = { "progress" },
+  --     lualine_z = { "location" },
+  --   },
+  --   inactive_sections = {
+  --     lualine_a = {},
+  --     lualine_b = {},
+  --     lualine_c = { "filename" },
+  --     lualine_x = { "location" },
+  --     lualine_y = {},
+  --     lualine_z = {},
+  --   },
+  --   tabline = {},
+  --   winbar = {},
+  --   inactive_winbar = {},
+  --   extensions = {},
+  -- },
 }
