557734d8fac961e9c35179c7842c9799be67eec4
ArianaEsperanza19 <ArianaEsperanza19>
2026-01-07 16:24:11 -0400

    Ajustar a nvim-lint para evitar que el linter de markdown estorbe en la escritura de lineas largas

---
+++ lua/plugins/nvim-lint.lua
@@ -0,0 +1,10 @@
+return {
+  {
+    "mfussenegger/nvim-lint",
+    opts = {
+      linters_by_ft = {
+        markdown = {}, -- Esto limpia los linters por defecto para markdown
+      },
+    },
+  },
+}
