45f3625d91822eaa8c394b6fc0bfae00761a0a9e
ArianaEsperanza19 <uribeatencio2021@gmail.com>
2026-01-20 18:35:53 -0400

    Agregar nuevo snippet para comandos de terminal en Rust y modificar filetypes aceptados

--- lua/filetypes/types.lua
+++ lua/filetypes/types.lua
@@ -1,9 +1,11 @@
 -- Autodetección de archivos Typst
 vim.filetype.add({
-	extension = {
-		typ = "typst",
-	},
+  extension = {
+    typ = "typst",
+    rst = "rust",
+  },
 })
 
 -- Opcional: Si quieres forzar la configuración para el cliente LSP
 vim.cmd([[autocmd BufNewFile,BufRead *.typ set filetype=typst]])
+vim.cmd([[autocmd BufRead,BufNewFile *.rst set filetype=rust]])
